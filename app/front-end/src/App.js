afd418088c5986d607da5fd53724059817f489fa
aleem3896 <aleem.3896@gmail.com>
2020-03-25 16:39:06 -0400

    Added Routing in App function to navigate to pages

--- app/front-end/src/App.js
+++ app/front-end/src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
+import SignupForm from './SignUp.js'
+import LoginForm from './Login.js'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  //<button onClick={this.Toggle.bind(this, check)} id='SingUpButton'>Sign Up</button>
+
+    return (
+      <div className="App">
+      <Router>
+      <Switch>
+        <header className="App-header">
+        <Route path="/Login">
+          <LoginForm/>
+          <Link to='/SignUp'>Sign Up</Link>
+        </Route>
+        <Route path="/SignUp">
+          <SignupForm/>
+          <Link to='/Login'>Login</Link>
+        </Route>
+        </header>
+      </Switch>
+      </Router>
+                
+      </div>
+    );
+  
+}
+
+function LoginForm(){
+
+    return(
+      <div>
+        <h2>Welcome</h2>
+        <h3>Sign In</h3>
+        <form id='login' method = 'POST'>
+        Email  <input type='email' id = 'inputEmail'name='email'/><br/>
+        Password<input type='password' id='inputPassword' name='loginEmail'/><br/>
+        <input type='submit' value='Sign In' />
+        </form>
+  
+      </div>
+  
+    );
+  
+
+}
+
+function SignUpForm(){
+  return(
+      <div>
+          <h2>Welcome</h2>
+          <h1>Create An Account</h1>
+          <form type='text' method='POST'>
+          First Name: <input type='text' id='firstName'/><br/>
+          Last Name: <input type='text' id='lastName' /><br/>
+          </form>
+      </div>
+  )
 }
 
 export default App;
