46ed6bbb8cc8fa0b14fefbaf6236a9301f3528e2
aleem3896 <aleem.3896@gmail.com>
2020-03-25 16:23:02 -0400

    Created Login and SignUp pages

---
+++ app/front-end/src/Login.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+function LoginForm(){
+  
+      return(
+        <div>
+          <h2>Welcome</h2>
+          <h3>Sign In</h3>
+          <form id='login' method = 'POST'>
+          Email  <input type='email' id = 'inputEmail'name='email'/><br/>
+          Password<input type='password' id='inputPassword' name='loginEmail'/><br/>
+          <input type='submit' value='Sign In' />
+          </form>
+    
+        </div>
+    
+      );
+    
+  
+  }
+
+  export default LoginForm;
