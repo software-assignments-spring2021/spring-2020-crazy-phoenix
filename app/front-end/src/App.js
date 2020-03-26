8e5d1fe867d950bbf888e5e5bf7799b2a9d32f87
aleem3896 <aleem.3896@gmail.com>
2020-03-25 23:47:00 -0400

    testing toolbar

--- app/front-end/src/App.js
+++ app/front-end/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import SignupForm from './SignUp.js'
 import LoginForm from './Login.js'
+import Toolbar from './toolbar.js'
 import './App.css';
 
 
@@ -12,20 +13,9 @@ function App() {
 
     return (
       <div className="App">
-      <Router>
-      <Switch>
-        <header className="App-header">
-        <Route path="/Login">
-          <LoginForm/>
-          <Link to='/SignUp'>Sign Up</Link>
-        </Route>
-        <Route path="/SignUp">
-          <SignupForm/>
-          <Link to='/Login'>Login</Link>
-        </Route>
-        </header>
-      </Switch>
-      </Router>
+      
+    
+      <Toolbar/>
                 
       </div>
     );
