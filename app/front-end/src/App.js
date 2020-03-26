b18aca45dffc62ce59f4902f597f1dd791522d13
SangeethVi <sv1468@nyu.edu>
2020-03-25 22:06:10 -0400

    Basic detailed route

--- app/front-end/src/App.js
+++ app/front-end/src/App.js
@@ -2,27 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './HomePage.js';
+import DetailedRoute from './DetailedRoute.js';
 
 function App() {
   return (
-    <HomePage></HomePage>
+    <DetailedRoute></DetailedRoute>
   );
 }
 
-// function DetailedRoute(){
-//   const min = 2;
-//   const max = 6;
-//   let numRoutes = Math.floor(Math.random() * (+max - +min)) + +min;
-//   routes = []
-//   const r = <div className = "Stop">Stop Name</div>
-//   for (let i=0; i<numRoutes; i++){
-//       routes.push(r)  
-//   }
-//   return(
-//     <div className = "Stops">
-//       {routes}
-//     </div>
-//   )
-// }
+
 
 export default App;
