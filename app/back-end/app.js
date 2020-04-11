1c7aea81e47aab8affd1c3ed84277695d3c32a26
Ayako Ohara <ayako.ohara@icloud.com>
2020-04-11 19:39:31 +0900

    fetch routes object

---
+++ app/back-end/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const fetch = require('node-fetch');
+const cors = require('cors');
+
+//app.use(cors);
+
+const sampleUrl = 'https://maps.googleapis.com/maps/api/directions/json?origin=Times+Square&destination=80+Lafayette+St,+New+York,+NY&mode=transit&key=API_KEY';
+
+app.get('/home', (req, res) => {
+  console.log('hellooooo');
+  res.send('hello!');
+});
+
+app.get('/data', (req, res) => {
+  fetch(sampleUrl, {method: "Get"})
+    .then(res => res.json())
+    .then((json) => {
+      const routesObject = json.routes;
+      res.send(routesObject);
+    });
+});
+
+
+module.exports = app;
