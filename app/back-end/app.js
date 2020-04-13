fce3c02756fb00a830d926374c7764f9dc4d4eae
Ayako Ohara <ayako.ohara@icloud.com>
2020-04-13 19:37:57 +0900

    more more mod

--- app/back-end/app.js
+++ app/back-end/app.js
@@ -6,7 +6,7 @@ const getAccessibleRouteList = require('./filter').getAccessibleRouteList;
 
 app.use(cors({ origin: 'http://localhost:3001', credentials: true }));
 
-const sampleUrl = 'https://maps.googleapis.com/maps/api/directions/json?origin=Columbia+University&destination=Hudson+Yards,+New+York,+NY&mode=transit&alternatives=true&key=AIzaSyDx0uVQTk3Ukph1ph9ppl97vC3M7rVKJ6U';
+const sampleUrl = 'https://maps.googleapis.com/maps/api/directions/json?origin=Columbia+University&destination=Hudson+Yards,+New+York,+NY&mode=transit&alternatives=true&key=API_KEY';
 
 app.get('/data', (req, res) => {
   fetch(sampleUrl, {method: "Get"})
