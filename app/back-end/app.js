bb2089f31edc1da268570d967d74de5277f8150b
SangeethVi <sv1468@nyu.edu>
2020-04-25 21:33:00 -0400

    Database connection done

--- app/back-end/app.js
+++ app/back-end/app.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 const getAccessibleRouteList = require('./filter').getAccessibleRouteList;
 const path = require('path');
 const fs = require('fs');
+require('dotenv').config();
+
 
 // passport
 const mongoose = require('mongoose');
@@ -12,11 +14,11 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
 app.use(cors({ origin: 'http://localhost:3001', credentials: true }));
-const fn = './config.json';
-const key = fs.readFileSync(fn);
-const conf = JSON.parse(key);
-const apiKey = conf.API_KEY;
-const sampleUrl = 'https://maps.googleapis.com/maps/api/directions/json?origin=Columbia+University&destination=Hudson+Yards,+New+York,+NY&mode=transit&alternatives=true&key='+apiKey;
+// const fn = './config.json';
+// const key = fs.readFileSync(fn);
+// const conf = JSON.parse(key);
+// const apiKey = conf.API_KEY;
+// const sampleUrl = 'https://maps.googleapis.com/maps/api/directions/json?origin=Columbia+University&destination=Hudson+Yards,+New+York,+NY&mode=transit&alternatives=true&key='+apiKey;
 
 // schema
 const User = require('./db.js');
@@ -24,16 +26,22 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// mongoose
-// avoid deprecation warning for old url parser
-mongoose.connect('mongodb://localhost/transportation', {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+
+// connecting to db
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_HOST = process.env.DB_HOST;
+const dbUrl = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}`;
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
   if (err) {
     console.log('Could not connect to database');
   }
+  console.log("Successfully connected to DB");
 });
 
 // avoid deprecation warning for collection.ensureIndex
 mongoose.set('useCreateIndex', true);
+
 
 app.get('/data', (req, res) => {
   fetch(sampleUrl, {method: "Get"})
