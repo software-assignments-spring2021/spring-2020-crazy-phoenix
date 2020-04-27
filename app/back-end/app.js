b4cab21d68f641392032298048ac5ce25d141530
aleem3896 <aleem.3896@gmail.com>
2020-04-27 02:32:21 -0400

    send data from HomePage to Backend

--- app/back-end/app.js
+++ app/back-end/app.js
@@ -8,6 +8,27 @@ app.use(cors({ origin: 'http://localhost:3001', credentials: true }));
 
 const sampleUrl = 'https://maps.googleapis.com/maps/api/directions/json?origin=Columbia+University&destination=Hudson+Yards,+New+York,+NY&mode=transit&alternatives=true&key=API_KEY';
 
+app.post('/data', (req, res) => {
+  
+  console.log("POST request received")
+  // const body = req.body
+  // console.log(req)
+
+  var requestBody = ""
+
+  req.on('data', function (data) {
+    requestBody += data;
+var jsonData = JSON.parse(requestBody);
+console.log(jsonData["origin"])
+console.log(jsonData["destination"])
+});
+  res.send('Success')
+
+
+});
+
+
+
 app.get('/data', (req, res) => {
   fetch(sampleUrl, {method: "Get"})
     .then(res => res.json())
