e81f481b5f3faf0f8f1a7f1c2c8555c95dce8d21
Ayako Ohara <ayako.ohara@icloud.com>
2020-05-01 16:23:25 +0900

    send route object from possibleRoutes to detailedRoute

--- app/front-end/src/possibleRoutes.js
+++ app/front-end/src/possibleRoutes.js
@@ -6,58 +6,38 @@ import DetailedRoute from './DetailedRoute';
 import queryString from 'querystring';
 import Redirect from "react-router-dom/es/Redirect";
 
-const getRoute = () => {
-  const possibleRoutes = [];
-  const subwayLines = ['1', '2', '3', '4', '5', '6', '7', 'A', 'C', 'E', 'B', 'D', 'F', 'M', 'G', 'N', 'Q', 'R', 'J', 'Z', 'L', 'S', 'G'];
-  let randomLength;
-
-  const randomRoutesListLength = Math.floor(Math.random()*(5-1))+1; // show 1-5 possible routes
-  for (let i = 0; i < randomRoutesListLength; i++) {
-    const route = [];
-    randomLength = Math.floor(Math.random()*(5-1)+1);
-    const subwayMax = subwayLines.length;
-    const subwayMin = 1;
-    for (let k = 0; k < randomLength; k++) {
-      const subwayIndex = Math.floor(Math.random()*(subwayMax-subwayMin)+subwayMin);
-      route.push(subwayLines[subwayIndex]);
-    }
-    possibleRoutes.push(route);
-  }
-
-  return possibleRoutes;
-};
-
-const addArrows = (route) => {
-  const newArray = ['->'];
-  for (let i = 0; i < route.length; i++) {
-    newArray.push(route[i]);
-    if (i < route.length-1) {
-      newArray.push('->');
+// return string of route to display
+const getShortRoute = (routeObjectString) => { console.log(routeObjectString);
+  const object = JSON.parse(routeObjectString);
+  const legs = object.legs;
+  let routePath = '';
+  for (let j = 0; j < legs.length; j++) {
+    const steps = legs[j].steps;
+    for (let k = 0; k < steps.length; k++) {
+      if (steps[k].travel_mode === 'TRANSIT') {
+        routePath += steps[k].transit_details.arrival_stop.name;
+        routePath += ' subway ---> ';
+        routePath += steps[k].transit_details.departure_stop.name;
+      } else {
+        routePath += steps[k].travel_mode;
+      }
+      routePath += '->';
     }
   }
-  newArray.push('->');
-  return newArray;
+  return routePath;
 };
 
 const PossibleRoutes = (props) => {
   // get origin and destination from /home
 
   const routes = props.location.state;
-  const handleClick = (event) => {
-    const detailedRoute = event.target.name;
-    props.action(detailedRoute);
-  };
-
-  const handleClickButton = (event) => {
-    //props.action(routes[0]);
-    console.log(routes);
-  }
-  const finalRoutesList = [];
-
+  // array of routes (object)
   const routesList = routes.routes;
+  /*
+  // array of routes (string)
+  const finalRoutesList = [];
   for (let i = 0; i < routesList.length; i++) {
     const legs = routesList[i].legs;
-
     let routePath = '';
     for (let j = 0; j < legs.length; j++) {
       const steps = legs[j].steps;
@@ -74,27 +54,29 @@ const PossibleRoutes = (props) => {
     }
     finalRoutesList.push(routePath);
   }
-  console.log(finalRoutesList);
+  console.log(finalRoutesList);*/
+
+
+  // route object array -> routeObjectString array (mapped)
+  const routeObjectStringArray = [];
+  for (let i = 0; i < routesList.length; i++) {
+    const string = JSON.stringify(routesList[i]);
+    routeObjectStringArray.push(string);
+  }
+
 
 
   return (
     <div className="possibleRoutes">
       <Link to="/Home">&#60;</Link>
-      
       <h1>possible routes</h1>
       <section className="content">
-        {finalRoutesList.map(route => (
-          <section className="route">{route}
-           <Link to={{pathname: '/Route', state: {route}}}>select</Link>
+        {routeObjectStringArray.map(routeString => (
+          <section className="route">{getShortRoute(routeString)}
+           <Link to={{pathname: '/Route', state: {routeString}}}>select</Link>
           </section>
         ))}
       </section>
-      <Link to={{
-        pathname: '/Route',
-        state: {routes: props.location.state}
-      }}>data transfer</Link>
-      <Link to='/Route' detailedRoute={routes} onClick={handleClickButton}>transfer data</Link>
-      <button onClick={handleClickButton}>transfer data</button>
     </div>
   );
 };
