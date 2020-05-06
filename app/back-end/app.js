8c476844d5f81bb7988e337af8a367cc0f40d621
Ayako Ohara <ayako.ohara@icloud.com>
2020-05-06 12:13:47 +0900

    erase unnecessary url

--- app/back-end/app.js
+++ app/back-end/app.js
@@ -18,7 +18,6 @@ const key = fs.readFileSync(fn);
 const conf = JSON.parse(key);
 //const apiKey = conf.API_KEY;
 const apiKey = process.env.API_KEY;
-//const sampleUrl = 'https://maps.googleapis.com/maps/api/directions/json?origin=Columbia+University&destination=Hudson+Yards,+New+York,+NY&mode=transit&alternatives=true&key='+apiKey;
 
 // schema
 const User = require('./db.js');
