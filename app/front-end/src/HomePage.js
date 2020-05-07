0e30f99e712befbabd59d5544bff85c1a0ef5c22
Ayako Ohara <ayako.ohara@icloud.com>
2020-05-07 16:59:47 +0900

    env

--- app/front-end/src/HomePage.js
+++ app/front-end/src/HomePage.js
@@ -7,7 +7,8 @@ import PossibleRoutes from './possibleRoutes';
 
 //const apiKey = key.API_KEY;
 
-const apiKey = process.env.API_KEY;
+//const apiKey = process.env.API_KEY;
+const apiKey = key.API_KEY;
 const mapUrl = 'https://maps.googleapis.com/maps/api/staticmap?center=New+York,NY&zoom=13&size=1200x800&maptype=roadmap&key=' + apiKey;
 
 
