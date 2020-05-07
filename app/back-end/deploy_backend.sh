501357561a8aaaf95ee650b2bd7eea8ca62d0eb2
Ayako Ohara <ayako.ohara@icloud.com>
2020-05-07 09:22:42 +0900

    modify sh for backend

--- app/back-end/deploy_backend.sh
+++ app/back-end/deploy_backend.sh
@@ -7,7 +7,7 @@ docker build -t "${IMAGE_NAME}:${IMAGE_TAG}" .
 docker tag "${IMAGE_NAME}:${IMAGE_TAG}" "${IMAGE_NAME}:latest"
 
 echo "Back-end authenticate and push image to Docker Hub"
-echo "${DOCKER_PASS}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+echo "${DOCKER_PASS}" | docker login -username "${DOCKER_USERNAME}" --password-stdin
 docker push "${IMAGE_NAME}:${IMAGE_TAG}"
 docker push "${IMAGE_NAME}:latest"
 
